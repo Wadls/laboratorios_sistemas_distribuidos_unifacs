184fa9314a50d745b5d53f8ea351a7383a3fde94
Sergio <wadsondaniel9@gmail.com>
2026-03-23 19:14:44 -0300

    Primeiro Laboratório

---
+++ Laboratorio 01/hello-world.js
@@ -0,0 +1,45 @@
+let txt = 'hello world';
+console.log(txt);
+
+let num1 = 10;
+let num2 = 5;
+
+//adição
+let soma = num1 + num2;
+console.log('Soma:', soma);
+
+//Subtração
+let subtracao = num1 - num2;
+console.log('Subtração:',subtracao);
+
+//Multiplicação
+let multiplicacao = num1 * num2;
+console.log('Multiplicação:', multiplicacao);
+
+//Divisão
+let divisao= num1 / num2;
+console.log('Divisao:', divisao);
+
+//Exponenciação
+let exponenciacao = num1 ** num2;
+console.log('Exponeciação:', exponenciacao);
+
+//Resto
+let resto = num1 % num2;
+console.log('Resto:', resto);
+
+//Definindo Constantes
+const PI =  3.14159;
+let raio = 5;
+
+//Calculando a area de um circulo
+let area = PI *raio * raio;
+console.log('Area do Circulo:', area);
+
+raio = 10
+//Calculando a area de um circulo
+area = PI *raio * raio;
+console.log('Area do Circulo:', area);
+
+//PI = 3.14;
+console.log("Fim do Script!");
